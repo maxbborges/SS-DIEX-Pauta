ba8e84e0958994056c3c972122b53593fcd32d41
Maxwell Borges Bezerra <max15borges@gmail.com>
2022-02-18 10:37:25 -0300

    Inclusão de campos para recuperação do dataset
    Inclusão de campos para recuperação do dataset

--- forms/5463 - Formulário de solicitacao_pauta_diex/custom/custom.js
+++ forms/5463 - Formulário de solicitacao_pauta_diex/custom/custom.js
@@ -3,5 +3,11 @@ $(document).ready(function () {
 });
 
 function init(){
+    if (ATIVIDADE==4||ATIVIDADE==0){
+        wdkAddChild('tbPauta')
+        $('[name="column1_1___1"]').text($('[name="solicitante"]').val())
+        $('[name="column2_1___1"]').text($('[name="ipCadastro"]').val())
+        $('[name="column3_1___1"]').text($('[name="ipSituacao"]').val())
+    }
     console.log("OK")
 }
---
+++ forms/5463 - Formulário de solicitacao_pauta_diex/events/displayFields.js
@@ -0,0 +1,17 @@
+function displayFields(form, customHTML) {
+	customHTML.append("<script> var ATIVIDADE = "+getValue("WKNumState")+";</script>"); 
+	customHTML.append("<script> var FORM_MODE = '" + form.getFormMode() + "';</script>");
+	var activity = getValue('WKNumState');
+	var usuario = fluigAPI.getUserService().getCurrent();
+	var data = new Date();
+	var mes = (data.getMonth()+1);
+    if (mes<=9){
+        mes = '0'+mes;
+    }
+
+	if(activity==0||activity==4){
+		form.setValue('solicitante',usuario.getFullName());
+    	form.setValue('ipCadastro',data.getDate()+'/'+mes+'/'+data.getFullYear());
+    	form.setValue('ipSituacao',"Novo");
+	} 
+}
